993818d4413bd797a25616d55e8499c6c59be09f
Chrys <t@theadmin.co.uk>
2022-05-26 15:51:05 +0100

    start of authentication

--- espMicroPy/espSource/main.py
+++ espMicroPy/espSource/main.py
@@ -4,10 +4,26 @@ import ubluetooth
 from esp32 import raw_temperature
 import machine
 
+from micropython import const
+_IRQ_CENTRAL_CONNECT                 = const(1 << 0)
+_IRQ_CENTRAL_DISCONNECT              = const(1 << 1)
+_IRQ_GATTS_WRITE                     = const(1 << 2)
+_IRQ_ENCRYPTION_UPDATE = const(28)
+
+_IRQ_PASSKEY_ACTION = const(31)
+_PASSKEY_ACTION_NONE = const(0)
+_PASSKEY_ACTION_INPUT = const(2)
+_PASSKEY_ACTION_DISPLAY = const(3)
+_PASSKEY_ACTION_NUMERIC_COMPARISON = const(4)
+
+keyFromRemote = '243280'
+keyFromLocal = '243281'
 class ManagedDevice:
     def __init__(self):
         self.state = 0
+# 0: Waiting for initial key
 
+# 1:
     def On_Message(self, msg):
         if self.state == 0:
             self.state = 1
@@ -33,6 +49,8 @@ class BLE():
         self.timer1 = Timer(0)
         self.timer2 = Timer(1)
 
+        self.ble.pa
+
         self.disconnected()
         self.ble.irq(self.ble_irq)
         self.register()
@@ -51,18 +69,18 @@ class BLE():
 
     def ble_irq(self, event, data):
 
-        if event == 1:
+        if event == _IRQ_CENTRAL_CONNECT:
             '''Central disconnected'''
             self.connected()
             self.ble.gatts_notify(0, self.tx, "data" + '\n')
             self.led(1)
 
-        elif event == 2:
+        elif event == _IRQ_CENTRAL_DISCONNECT:
             '''Central disconnected'''
             self.advertiser()
             self.disconnected()
 
-        elif event == 3:
+        elif event == _IRQ_GATTS_WRITE:
             '''New message received'''
 
             buffer = self.ble.gatts_read(self.rx)
@@ -76,6 +94,21 @@ class BLE():
                 self.ble.gatts_notify(0, self.tx, msg_res + '\n')
             if message == 'blue_led':
                 blue_led.value(not blue_led.value())
+        elif event == _IRQ_PASSKEY_ACTION:
+            conn_handle, action, passkey = data
+            print("passkey action", conn_handle, action, passkey)
+            # passkey will be non-zero if action is "numeric comparison".
+            if action == _PASSKEY_ACTION_NONE:
+                pass
+            elif action == _PASSKEY_ACTION_INPUT:
+                self._ble.gap_passkey(conn_handle, action, keyFromRemote)
+            elif action == _PASSKEY_ACTION_DISPLAY:
+                self._ble.gap_passkey(conn_handle, action, keyFromLocal)
+                pass
+            elif action == _PASSKEY_ACTION_NUMERIC_COMPARISON:
+                self._ble.gap_passkey(conn_handle, action, passkey == keyFromLocal)
+                pass
+
 
     def register(self):
 
